9007f8bfc977311614d01b5940d59e88a1aab8ee
toooooooby <toby.net.info.mail+git@gmail.com>
2013-05-01 01:37:04 +0900

    modularization and writing test

--- lib/kagami.rb
+++ lib/kagami.rb
@@ -1,5 +1,39 @@
 require "kagami/version"
 
 module Kagami
-  # Your code goes here...
+  class << self
+    def max_width(line)
+      return line.lines.map{|x| x.chomp.size}.max
+    end
+
+    def ljust(line)
+      width = max_width(line)
+      return line.lines.map{|x| x.chomp.ljust(width)}.join("\n")
+    end
+
+    def reversed(line)
+      return line.lines.map{|x| x.chomp.reverse}.join("\n")
+    end
+
+    def readable(line)
+      return line.gsub(/(\w+(:? \w+)*)/){|s| s.reverse}
+    end
+
+    def convert(line)
+      # Align string
+      aligned_line = ljust(line)
+
+      # Reverse
+      reversed_line = reversed(aligned_line)
+
+      # Rereverse readable words only!!!!!
+      readable_line = readable(reversed_line)
+
+      return readable_line
+    end
+
+    def run(source)
+      puts convert(source)
+    end
+  end
 end
---
+++ test/test_kagami.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+$VERBOSE = true
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'kagami'
+
+describe "Kagami" do
+  ONE_LINE_DATA = "The internet >>> Ruby >>>> Java and you"
+  MULTI_LINE_DATA = <<-EOD
+                      Firewall
+                         ||
++--------+  +-------+    || The Internet 
+| server +--+ proxy +----||======☁=======
++--------+  +-------+    ||
+                         ||
+  EOD
+
+  it ".max_width" do
+    Kagami.max_width(ONE_LINE_DATA).must_equal 39
+    Kagami.max_width(MULTI_LINE_DATA).must_equal 41
+  end
+
+  it ".ljust" do
+    Kagami.ljust(ONE_LINE_DATA).must_equal ONE_LINE_DATA
+    Kagami.ljust(MULTI_LINE_DATA).must_equal <<-EOD.chomp
+                      Firewall           
+                         ||              
++--------+  +-------+    || The Internet 
+| server +--+ proxy +----||======☁=======
++--------+  +-------+    ||              
+                         ||              
+    EOD
+  end
+
+  it ".reversed" do
+    Kagami.reversed(ONE_LINE_DATA).must_equal "uoy dna avaJ >>>> ybuR >>> tenretni ehT"
+    Kagami.reversed(MULTI_LINE_DATA).must_equal <<-EOD.chomp
+llaweriF                      
+||                         
+ tenretnI ehT ||    +-------+  +--------+
+=======☁======||----+ yxorp +--+ revres |
+||    +-------+  +--------+
+||                         
+    EOD
+  end
+
+  it ".readable_after_reverse" do
+    Kagami.readable(ONE_LINE_DATA).must_equal "tenretni ehT >>> ybuR >>>> uoy dna avaJ"
+    Kagami.readable(MULTI_LINE_DATA).must_equal <<-EOD
+                      llaweriF
+                         ||
++--------+  +-------+    || tenretnI ehT 
+| revres +--+ yxorp +----||======☁=======
++--------+  +-------+    ||
+                         ||
+    EOD
+  end
+
+  it ".convert" do
+    Kagami.convert(ONE_LINE_DATA).must_equal "Java and you >>>> Ruby >>> The internet"
+    Kagami.convert(MULTI_LINE_DATA).must_equal <<-EOD.chomp
+           Firewall                      
+              ||                         
+ The Internet ||    +-------+  +--------+
+=======☁======||----+ proxy +--+ server |
+              ||    +-------+  +--------+
+              ||                         
+    EOD
+  end
+end
